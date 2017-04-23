d2810fc3e1ee932049deb38f9319f74c216349c7
afritz1 <afritz1@users.noreply.github.com>
2017-04-23 13:13:43 -0700

    Added code for weapon animations.
    Each weapon animation appears to have five frames dedicated to each
    swing direction, and three frames for either sheathing or unsheathing
    (one of which is used for the idle animation).

---
+++ OpenTESArena/src/Entities/WeaponAnimation.cpp
@@ -0,0 +1,167 @@
+#include <cassert>
+#include <map>
+
+#include "WeaponAnimation.h"
+
+#include "../Items/WeaponType.h"
+#include "../Utilities/Debug.h"
+
+namespace
+{
+	// Mappings of weapon types to .CIF filenames.
+	const std::map<WeaponType, std::string> WeaponTypeFilenames =
+	{
+		{ WeaponType::BattleAxe, "AXE" },
+		{ WeaponType::Broadsword, "SWORD" },
+		{ WeaponType::Claymore, "SWORD" },
+		{ WeaponType::Dagger, "SWORD" },
+		{ WeaponType::DaiKatana, "SWORD" },
+		{ WeaponType::Fists, "HAND" },
+		{ WeaponType::Flail, "STAR" },
+		{ WeaponType::Katana, "SWORD" },
+		{ WeaponType::LongBow, "" }, // Bows have no animations.
+		{ WeaponType::Longsword, "SWORD" },
+		{ WeaponType::Mace, "MACE" },
+		{ WeaponType::Saber, "SWORD" },
+		{ WeaponType::ShortBow, "" }, // Bows have no animations.
+		{ WeaponType::Shortsword, "SWORD" },
+		{ WeaponType::Staff, "STAFF" },
+		{ WeaponType::Tanto, "SWORD" },
+		{ WeaponType::Wakizashi, "SWORD" },
+		{ WeaponType::WarAxe, "AXE" },
+		{ WeaponType::Warhammer, "HAMMER" }
+	};
+
+	// Mappings of weapon animation states to ranges of frame indices, excluding fists.
+	const std::map<WeaponAnimation::State, std::vector<int>> WeaponAnimationRanges =
+	{
+		{ WeaponAnimation::State::Sheathed, { } },
+		{ WeaponAnimation::State::Unsheathing, { 30, 31, 32 } },
+		{ WeaponAnimation::State::Idle, { 32 } },
+		{ WeaponAnimation::State::Forward, { 25, 26, 27, 28, 29 } },
+		{ WeaponAnimation::State::Down, { 0, 1, 2, 3, 4 } },
+		{ WeaponAnimation::State::Right, { 15, 16, 17, 18, 19 } },
+		{ WeaponAnimation::State::Left, { 10, 11, 12, 13, 14 } },
+		{ WeaponAnimation::State::DownRight, { 20, 21, 22, 23, 24 } },
+		{ WeaponAnimation::State::DownLeft, { 5, 6, 7, 8, 9 } },
+		{ WeaponAnimation::State::Sheathing, { 32, 31, 30 } }
+	};
+
+	// Mappings of fists animation states to ranges of frame indices.
+	const std::map<WeaponAnimation::State, std::vector<int>> FistsAnimationRanges =
+	{
+		{ WeaponAnimation::State::Sheathed, { } },
+		{ WeaponAnimation::State::Unsheathing, { 10, 11, 12 } },
+		{ WeaponAnimation::State::Idle, { 12 } },
+		{ WeaponAnimation::State::Forward, { 0, 1, 2, 3, 4 } },
+		{ WeaponAnimation::State::Down, { 0, 1, 2, 3, 4 } },
+		{ WeaponAnimation::State::Right, { 5, 6, 7, 8, 9 } },
+		{ WeaponAnimation::State::Left, { 5, 6, 7, 8, 9 } }, // Verify some of these ranges.
+		{ WeaponAnimation::State::DownRight, { 5, 6, 7, 8, 9 } },
+		{ WeaponAnimation::State::DownLeft, { 5, 6, 7, 8, 9 } },
+		{ WeaponAnimation::State::Sheathing, { 12, 11, 10 } }
+	};
+}
+
+const double WeaponAnimation::DEFAULT_TIME_PER_FRAME = 0.20;
+
+WeaponAnimation::WeaponAnimation(WeaponType weaponType)
+{
+	// Bows are not allowed because they have no animation.
+	Debug::check((weaponType != WeaponType::LongBow) && (weaponType != WeaponType::ShortBow),
+		"Weapon Animation", "Bows do not have weapon animations.");
+
+	this->state = WeaponAnimation::State::Sheathed;
+	this->weaponType = weaponType;
+	this->currentTime = 0.0;
+	this->timePerFrame = WeaponAnimation::DEFAULT_TIME_PER_FRAME;
+	this->rangeIndex = 0;
+}
+
+WeaponAnimation::~WeaponAnimation()
+{
+
+}
+
+const std::vector<int> &WeaponAnimation::getCurrentRange() const
+{
+	const std::vector<int> &indices = (this->weaponType == WeaponType::Fists) ?
+		FistsAnimationRanges.at(this->state) : WeaponAnimationRanges.at(this->state);
+	return indices;
+}
+
+bool WeaponAnimation::isSheathed() const
+{
+	return this->state == WeaponAnimation::State::Sheathed;
+}
+
+const std::string &WeaponAnimation::getAnimationFilename() const
+{
+	const std::string &filename = WeaponTypeFilenames.at(this->weaponType);
+	return filename;
+}
+
+int WeaponAnimation::getFrameIndex() const
+{
+	// The sheathe animation's frame index should not be used.
+	assert(!this->isSheathed());
+
+	const std::vector<int> &indices = this->getCurrentRange();
+	return indices.at(this->rangeIndex);
+}
+
+void WeaponAnimation::setState(WeaponAnimation::State state)
+{
+	this->state = state;
+
+	// Switch to the beginning of the new range of indices if not sheathed.
+	if (state != WeaponAnimation::State::Sheathed)
+	{
+		const std::vector<int> &indices = this->getCurrentRange();
+		this->rangeIndex = indices.front();
+	}
+	else
+	{
+		// This is the "bad" index during sheathed state. It references a valid frame,
+		// but it is not displayed, so it shouldn't be accessed.
+		this->rangeIndex = 0;
+	}
+}
+
+void WeaponAnimation::tick(double dt)
+{
+	// Tick if not idle and not sheathed.
+	if ((this->state != WeaponAnimation::State::Idle) &&
+		(this->state != WeaponAnimation::State::Sheathed))
+	{
+		this->currentTime += dt;
+
+		// Update the index if current time has passed the time per frame.
+		if (this->currentTime >= this->timePerFrame)
+		{
+			this->currentTime = 0.0;
+			this->rangeIndex++;
+
+			// Get the current range of frame indices.
+			const std::vector<int> &indices = this->getCurrentRange();
+
+			// If the index is outside the range, decide which state is next.
+			if (this->rangeIndex >= indices.size())
+			{
+				if (this->state == WeaponAnimation::State::Sheathing)
+				{
+					// Switching from sheathing to sheathed. The range index isn't used in 
+					// sheathed state.
+					this->state = WeaponAnimation::State::Sheathed;
+					this->rangeIndex = 0;
+				}
+				else
+				{
+					// Switching from unsheathing to idle, or from swing to idle.
+					this->state = WeaponAnimation::State::Idle;
+					this->rangeIndex = WeaponAnimationRanges.at(this->state).front();
+				}
+			}
+		}
+	}
+}
---
+++ OpenTESArena/src/Entities/WeaponAnimation.h
@@ -0,0 +1,70 @@
+#ifndef WEAPON_ANIMATION_H
+#define WEAPON_ANIMATION_H
+
+#include <cstddef>
+#include <string>
+#include <vector>
+
+// Stores the current state of the player's weapon animation.
+
+// Since Arena's weapon animations mostly share the same ordering, they can be hardcoded.
+// Fists are an exception because they have fewer frames.
+
+// The bow should not be used here because there is no bow animation in the Arena files;
+// just a single idle frame that disappears when firing an arrow.
+
+enum class WeaponType;
+
+class WeaponAnimation
+{
+public:
+	enum class State
+	{
+		Sheathed, // Not displayed on-screen.
+		Unsheathing,
+		Idle,
+		Forward,
+		Down,
+		Right,
+		Left,
+		DownRight,
+		DownLeft,
+		Sheathing // Reverse of unsheathing animation.
+	};
+private:
+	// Default time spent per animation frame.
+	static const double DEFAULT_TIME_PER_FRAME;
+
+	WeaponAnimation::State state;
+	WeaponType weaponType;
+	double currentTime, timePerFrame;
+	size_t rangeIndex;
+
+	// Gets the range of indices associated with the current animation state.
+	const std::vector<int> &getCurrentRange() const;
+public:
+	WeaponAnimation(WeaponType weaponType);
+	~WeaponAnimation();
+
+	// Returns whether the weapon is currently displayed.
+	bool isSheathed() const;
+
+	// Gets the filename associated with the weapon (i.e., AXE, HAMMER, etc.).
+	// This is used with the current index to determine which frame is drawn.
+	const std::string &getAnimationFilename() const;
+
+	// Gets the index into the .CIF animation's current frame. Do not call this
+	// method if the sheathed animation is active.
+	int getFrameIndex() const;
+
+	// Sets the current weapon state. This resets the current animation index to the
+	// beginning of the new state's range.
+	void setState(WeaponAnimation::State state);
+
+	// Ticks the weapon animation by delta time. If the weapon animation is swinging or
+	// unsheathing, it will return to the idle animation automatically. If sheathing, it 
+	// will return to the sheathed state automatically.
+	void tick(double dt);
+};
+
+#endif
