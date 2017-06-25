0f5f0d49078f980f88f57013f95459110e8df6da
afritz1 <afritz1@users.noreply.github.com>
2017-06-24 22:55:26 -0700

    Added CITYDATA.00 parser.
    CITYDATA.00 contains the names and screen coordinates of all locations
    for every province on the world map. There is some unknown data, and I'm
    not sure if it could be the random seeds for each location, or maybe
    coordinates for random dungeons, or maybe something else. Anyway, there
    should be enough data now to draw the icons and names of each location
    on the world map.

---
+++ OpenTESArena/src/Assets/CityDataFile.cpp
@@ -0,0 +1,102 @@
+#include <cstring>
+
+#include "CityDataFile.h"
+
+#include "../Utilities/Bytes.h"
+#include "../Utilities/Debug.h"
+
+#include "components/vfs/manager.hpp"
+
+const int CityDataFile::PROVINCE_COUNT = 9;
+
+CityDataFile::CityDataFile(const std::string &filename)
+{
+	VFS::IStreamPtr stream = VFS::Manager::get().open(filename.c_str());
+	DebugAssert(stream != nullptr, "Could not open \"" + filename + "\".");
+
+	stream->seekg(0, std::ios::end);
+	const auto fileSize = stream->tellg();
+	stream->seekg(0, std::ios::beg);
+
+	std::vector<uint8_t> srcData(fileSize);
+	stream->read(reinterpret_cast<char*>(srcData.data()), srcData.size());
+
+	// Size of each province definition in bytes.
+	const size_t provinceDataSize = 1228;
+
+	// Size of each location definition in bytes.
+	const size_t locationDataSize = 24;
+
+	// Iterate over each province and initialize the location data.
+	for (size_t i = 0; i < this->provinces.size(); i++)
+	{
+		const size_t startOffset = provinceDataSize * i;
+		auto &province = this->provinces.at(i);
+
+		// Read the province header.
+		const uint8_t *provinceNamePtr = srcData.data() + startOffset;
+		std::memcpy(province.name.data(), provinceNamePtr, province.name.size());
+		std::memcpy(province.unknown.data(), 
+			provinceNamePtr + province.name.size(), province.unknown.size());
+
+		const uint8_t *locationPtr = provinceNamePtr + province.name.size() + 
+			province.unknown.size();
+
+		for (auto &cityState : province.cityStates)
+		{
+			// Read the city-state data (name, x, y).
+			std::memcpy(cityState.name.data(), locationPtr, cityState.name.size());
+			cityState.x = Bytes::getLE16(locationPtr + cityState.name.size());
+			cityState.y = Bytes::getLE16(locationPtr + cityState.name.size() + sizeof(cityState.x));
+			locationPtr += locationDataSize + 1; // Add 1 for null-terminator.
+		}
+
+		for (auto &town : province.towns)
+		{
+			// Read the town data (name, x, y).
+			std::memcpy(town.name.data(), locationPtr, town.name.size());
+			town.x = Bytes::getLE16(locationPtr + town.name.size());
+			town.y = Bytes::getLE16(locationPtr + town.name.size() + sizeof(town.x));
+			locationPtr += locationDataSize + 1;
+		}
+
+		for (auto &village : province.villages)
+		{
+			// Read the village data (name, x, y).
+			std::memcpy(village.name.data(), locationPtr, village.name.size());
+			village.x = Bytes::getLE16(locationPtr + village.name.size());
+			village.y = Bytes::getLE16(locationPtr + village.name.size() + sizeof(village.x));
+			locationPtr += locationDataSize + 1;
+		}
+
+		// Read the dungeon data (name, x, y). The second dungeon is listed first.
+		std::memcpy(province.secondDungeon.name.data(), locationPtr, 
+			province.secondDungeon.name.size());
+		province.secondDungeon.x = Bytes::getLE16(locationPtr + province.secondDungeon.name.size());
+		province.secondDungeon.y = Bytes::getLE16(locationPtr + 
+			province.secondDungeon.name.size() + sizeof(province.secondDungeon.x));
+		locationPtr += locationDataSize + 1;
+
+		std::memcpy(province.firstDungeon.name.data(), locationPtr, 
+			province.firstDungeon.name.size());
+		province.firstDungeon.x = Bytes::getLE16(locationPtr + province.firstDungeon.name.size());
+		province.firstDungeon.y = Bytes::getLE16(locationPtr +
+			province.firstDungeon.name.size() + sizeof(province.firstDungeon.x));
+		locationPtr += locationDataSize + 1;
+
+		// Read unknown data.
+		std::memcpy(province.unknownData.data.data(), locationPtr, province.unknownData.data.size());
+	}
+}
+
+CityDataFile::~CityDataFile()
+{
+
+}
+
+const CityDataFile::ProvinceData &CityDataFile::getProvinceData(int index)
+{
+	DebugAssert(index < CityDataFile::PROVINCE_COUNT, "Province index out of range.");
+
+	return this->provinces.at(index);
+}
---
+++ OpenTESArena/src/Assets/CityDataFile.h
@@ -0,0 +1,56 @@
+#ifndef CITY_DATA_FILE_H
+#define CITY_DATA_FILE_H
+
+#include <array>
+#include <cstdint>
+#include <string>
+
+// CITYDATA.00 contains basic data for locations in each province on the world map.
+// It has the names of each place and their XY coordinates on the screen.
+
+// CITYDATA.64 and CITYDATA.65 are most likely duplicates of CITYDATA.00.
+
+class CityDataFile
+{
+public:
+	// Each province contains 8 city-states, 8 towns, 16 villages, and two main quest dungeons.
+	// The center province is an exception; it has just 1 city (all others are zeroed out).
+	struct ProvinceData
+	{
+		struct LocationData
+		{
+			std::array<char, 20> name; // Null-terminated.
+			uint16_t x, y; // Position on screen.
+		};
+
+		// There is some unknown data, and it's 384 bytes per province, which would appear
+		// to be 12 bytes per location. Perhaps they are seed values, or maybe coordinates for
+		// random dungeons? Climate IDs (grassy, desert, cold, snowy)?
+		struct UnknownData
+		{
+			std::array<char, 384> data;
+		};
+
+		std::array<char, 20> name; // Null-terminated.
+		std::array<char, 8> unknown; // Province-relevant data...?
+		std::array<LocationData, 8> cityStates;
+		std::array<LocationData, 8> towns;
+		std::array<LocationData, 16> villages;
+		LocationData secondDungeon;
+		LocationData firstDungeon;
+		UnknownData unknownData;
+	};
+private:
+	// These are ordered the same as usual (read left to right, and center is last).
+	std::array<ProvinceData, 9> provinces;
+public:
+	CityDataFile(const std::string &filename);
+	~CityDataFile();
+
+	static const int PROVINCE_COUNT;
+
+	// Gets the province data at the given province index.
+	const CityDataFile::ProvinceData &getProvinceData(int index);
+};
+
+#endif
