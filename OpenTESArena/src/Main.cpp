4f28512b85e61e23acfd78bced87129a24e1dbf4
afritz1 <afritz1@users.noreply.github.com>
2018-04-19 21:33:58 -0700

    Added top-level exception handling.
    Any time an exception is thrown in the Game constructor or the Game
    loop, it'll be handled in the catch block in main().

--- OpenTESArena/src/Main.cpp
+++ OpenTESArena/src/Main.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
+#include <stdexcept>
+#include <string>
 
 #include "SDL.h"
 
 #include "Game/Game.h"
+#include "Utilities/Debug.h"
 
 int main(int argc, char *argv[])
 {
-	// It shouldn't be taking any command line arguments; it loads from text files.
 	static_cast<void>(argc);
 	static_cast<void>(argv);
 
-	Game g;
-	g.loop();
+	try
+	{
+		Game g;
+		g.loop();
+	}
+	catch (const std::exception &e)
+	{
+		DebugCrash("Exception! " + std::string(e.what()));
+	}
 
 	return EXIT_SUCCESS;
 }
--- OpenTESArena/src/Utilities/Debug.h
+++ OpenTESArena/src/Utilities/Debug.h
@@ -1,6 +1,7 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
+#include <stdexcept>
 #include <string>
 
 // Below are various debug methods and macros for replacing asserts or program exits 
@@ -22,13 +23,13 @@ private:
 	Debug() = delete;
 	~Debug() = delete;
 
-	// Shortens the __FILE__ macro so it only includes a couple parent folders.
-	static std::string getShorterPath(const char *__file__);
-
 	// Writes a debug message to the console with the file path and line number.
 	static void write(Debug::MessageType type, const std::string &filePath,
 		int lineNumber, const std::string &message);
 public:
+	// Shortens the __FILE__ macro so it only includes a couple parent folders.
+	static std::string getShorterPath(const char *__file__);
+
 	// Use DebugMention() instead. Helper method for mentioning something about program state.
 	static void mention(const char *__file__, int lineNumber, const std::string &message);
 
@@ -40,13 +41,15 @@ public:
 
 	// Use DebugAssert() instead. Helper method for verifying that a condition is true, and
 	// crashing the program if it is false.
-	static void check(bool condition, const char *__file__, int lineNumber, 
+	static void check(bool condition, const char *__file__, int lineNumber,
 		const std::string &message);
 
 #define DebugMention(message) Debug::mention(__FILE__, __LINE__, message)
 #define DebugWarning(message) Debug::warning(__FILE__, __LINE__, message)
 #define DebugCrash(message) Debug::crash(__FILE__, __LINE__, message)
 #define DebugAssert(condition, message) Debug::check(condition, __FILE__, __LINE__, message)
+#define DebugException(message) std::runtime_error(std::string(message) + " (" + \
+	Debug::getShorterPath(__FILE__) + "(" + std::to_string(__LINE__) + "))")
 #define DebugNotImplemented() Debug::crash(__FILE__, __LINE__, "Not implemented.")
 };
 
