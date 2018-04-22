0e035c2844c3f73cda56b32acfdc75a6f62c2d34
afritz1 <afritz1@users.noreply.github.com>
2018-04-21 21:32:11 -0700

    Added save method stubs to ArenaSave.

--- OpenTESArena/src/Assets/ArenaSave.cpp
+++ OpenTESArena/src/Assets/ArenaSave.cpp
@@ -2,51 +2,93 @@
 #include "../Utilities/Debug.h"
 #include "../Utilities/File.h"
 
-ArenaTypes::Automap ArenaSave::loadAUTOMAP(int index)
+ArenaTypes::Automap ArenaSave::loadAUTOMAP(const std::string &filename, int index)
 {
 	ArenaTypes::Automap automap;
 	DebugNotImplemented();
 	return automap;
 }
 
-ArenaTypes::Log ArenaSave::loadLOG(int index)
+ArenaTypes::Log ArenaSave::loadLOG(const std::string &filename, int index)
 {
 	ArenaTypes::Log log;
 	DebugNotImplemented();
 	return log;
 }
 
-ArenaTypes::SaveEngine ArenaSave::loadSAVEENGN(int index)
+ArenaTypes::SaveEngine ArenaSave::loadSAVEENGN(const std::string &filename, int index)
 {
 	ArenaTypes::SaveEngine saveEngine;
 	DebugNotImplemented();
 	return saveEngine;
 }
 
-ArenaTypes::SaveGame ArenaSave::loadSAVEGAME(int index)
+ArenaTypes::SaveGame ArenaSave::loadSAVEGAME(const std::string &filename, int index)
 {
 	ArenaTypes::SaveGame saveGame;
 	DebugNotImplemented();
 	return saveGame;
 }
 
-ArenaTypes::SpellData ArenaSave::loadSPELLS(int index)
+ArenaTypes::Spells ArenaSave::loadSPELLS(const std::string &filename, int index)
 {
-	ArenaTypes::SpellData spellData;
+	ArenaTypes::Spells spells;
 	DebugNotImplemented();
-	return spellData;
+	return spells;
 }
 
-ArenaTypes::SpellData ArenaSave::loadSPELLSG(int index)
+ArenaTypes::Spells ArenaSave::loadSPELLSG(const std::string &filename, int index)
 {
-	ArenaTypes::SpellData spellData;
+	ArenaTypes::Spells spells;
 	DebugNotImplemented();
-	return spellData;
+	return spells;
 }
 
-ArenaTypes::MQLevelState ArenaSave::loadSTATE(int index)
+ArenaTypes::MQLevelState ArenaSave::loadSTATE(const std::string &filename, int index)
 {
 	ArenaTypes::MQLevelState state;
 	DebugNotImplemented();
 	return state;
+}
+
+void ArenaSave::saveAUTOMAP(const std::string &filename, int index,
+	const ArenaTypes::Automap &data)
+{
+	DebugNotImplemented();
+}
+
+void ArenaSave::saveLOG(const std::string &filename, int index,
+	const ArenaTypes::Log &data)
+{
+	DebugNotImplemented();
+}
+
+void ArenaSave::saveSAVEENGN(const std::string &filename, int index,
+	const ArenaTypes::SaveEngine &data)
+{
+	DebugNotImplemented();
+}
+
+void ArenaSave::saveSAVEGAME(const std::string &filename, int index,
+	const ArenaTypes::SaveGame &data)
+{
+	DebugNotImplemented();
+}
+
+void ArenaSave::saveSPELLS(const std::string &filename, int index,
+	const ArenaTypes::Spells &data)
+{
+	DebugNotImplemented();
+}
+
+void ArenaSave::saveSPELLSG(const std::string &filename, int index,
+	const ArenaTypes::Spells &data)
+{
+	DebugNotImplemented();
+}
+
+void ArenaSave::saveSTATE(const std::string &filename, int index,
+	const ArenaTypes::MQLevelState &data)
+{
+	DebugNotImplemented();
 }
--- OpenTESArena/src/Assets/ArenaSave.h
+++ OpenTESArena/src/Assets/ArenaSave.h
@@ -1,6 +1,8 @@
 #ifndef ARENA_SAVE_H
 #define ARENA_SAVE_H
 
+#include <string>
+
 #include "ArenaTypes.h"
 
 // Loads various files saved by the original game.
@@ -11,16 +13,34 @@ private:
 	ArenaSave() = delete;
 	~ArenaSave() = delete;
 public:
-	static ArenaTypes::Automap loadAUTOMAP(int index);
-	static ArenaTypes::Log loadLOG(int index);
-	static ArenaTypes::SaveEngine loadSAVEENGN(int index);
-	static ArenaTypes::SaveGame loadSAVEGAME(int index);
-	static ArenaTypes::SpellData loadSPELLS(int index);
-	static ArenaTypes::SpellData loadSPELLSG(int index);
-	static ArenaTypes::MQLevelState loadSTATE(int index);
+	static ArenaTypes::Automap loadAUTOMAP(const std::string &filename, int index);
+	static ArenaTypes::Log loadLOG(const std::string &filename, int index);
+	static ArenaTypes::SaveEngine loadSAVEENGN(const std::string &filename, int index);
+	static ArenaTypes::SaveGame loadSAVEGAME(const std::string &filename, int index);
+	static ArenaTypes::Spells loadSPELLS(const std::string &filename, int index);
+	static ArenaTypes::Spells loadSPELLSG(const std::string &filename, int index);
+	static ArenaTypes::MQLevelState loadSTATE(const std::string &filename, int index);
 	// To do: load city data.
 	// To do: load INN.0x.
 	// To do: load wild 001, 002, 003, 004.
+
+	static void saveAUTOMAP(const std::string &filename, int index,
+		const ArenaTypes::Automap &data);
+	static void saveLOG(const std::string &filename, int index,
+		const ArenaTypes::Log &data);
+	static void saveSAVEENGN(const std::string &filename, int index,
+		const ArenaTypes::SaveEngine &data);
+	static void saveSAVEGAME(const std::string &filename, int index,
+		const ArenaTypes::SaveGame &data);
+	static void saveSPELLS(const std::string &filename, int index,
+		const ArenaTypes::Spells &data);
+	static void saveSPELLSG(const std::string &filename, int index,
+		const ArenaTypes::Spells &data);
+	static void saveSTATE(const std::string &filename, int index,
+		const ArenaTypes::MQLevelState &data);
+	// To do: save city data.
+	// To do: save INN.0x.
+	// To do: save wild 001, 002, 003, 004.
 };
 
 #endif
