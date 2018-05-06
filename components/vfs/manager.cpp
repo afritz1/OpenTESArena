5ccc15e7f560d11901d70eb727785e4c311f46bf
afritz1 <afritz1@users.noreply.github.com>
2018-05-06 13:21:25 -0700

    Added inGlobalBSA reference parameter to VFS open().
    This lets the caller see if the requested file was opened in the
    filesystem or in GLOBAL.BSA (useful for classes like INFFile that only
    run decryption on .INF files in a .BSA).

--- components/vfs/manager.cpp
+++ components/vfs/manager.cpp
@@ -1,4 +1,3 @@
-
 #include "manager.hpp"
 
 #ifdef _WIN32
@@ -19,15 +18,11 @@
 
 #include "../archives/bsaarchive.hpp"
 
-
 namespace
 {
-
-std::vector<std::string> gRootPaths;
-Archives::BsaArchive gGlobalBsa;
-
+	std::vector<std::string> gRootPaths;
+	Archives::BsaArchive gGlobalBsa;
 }
-
 
 namespace VFS
 {
@@ -38,42 +33,67 @@ Manager::Manager()
 
 void Manager::initialize(std::string&& root_path)
 {
-    if(root_path.empty())
-        root_path += "./";
-    else if(root_path.back() != '/' && root_path.back() != '\\')
-        root_path += "/";
+	if (root_path.empty())
+		root_path += "./";
+	else if (root_path.back() != '/' && root_path.back() != '\\')
+		root_path += "/";
 
-    gGlobalBsa.load(root_path+"GLOBAL.BSA");
+	gGlobalBsa.load(root_path + "GLOBAL.BSA");
 
-    gRootPaths.push_back(std::move(root_path));
+	gRootPaths.push_back(std::move(root_path));
 }
 
 void Manager::addDataPath(std::string&& path)
 {
-    if(path.empty())
-        path += "./";
-    else if(path.back() != '/' && path.back() != '\\')
-        path += "/";
-    gRootPaths.push_back(std::move(path));
+	if (path.empty())
+		path += "./";
+	else if (path.back() != '/' && path.back() != '\\')
+		path += "/";
+	gRootPaths.push_back(std::move(path));
 }
 
+IStreamPtr Manager::open(const char *name, bool &inGlobalBSA)
+{
+	std::unique_ptr<std::ifstream> stream(new std::ifstream());
+
+	// Search in reverse, so newer paths take precedence.
+	const auto iter = std::find_if(gRootPaths.rbegin(), gRootPaths.rend(),
+		[name, &stream](const std::string &rootPath)
+	{
+		stream->open(rootPath + name, std::ios::binary);
+		return stream->good();
+	});
+
+	if (iter != gRootPaths.rend())
+	{
+		inGlobalBSA = false;
+		return IStreamPtr(std::move(stream));
+	}
+	else
+	{
+		inGlobalBSA = true;
+		return gGlobalBsa.open(name);
+	}
+}
 
 IStreamPtr Manager::open(const char *name)
 {
-    std::unique_ptr<std::ifstream> stream(new std::ifstream());
-    // Search in reverse, so newer paths take precedence.
-    auto piter = gRootPaths.rbegin();
-    while(piter != gRootPaths.rend())
-    {
-        stream->open((*piter+name).c_str(), std::ios_base::binary);
-        if(stream->good()) return IStreamPtr(std::move(stream));
-        ++piter;
-    }
-
-    return gGlobalBsa.open(name);
+	bool dummy;
+	return open(name, dummy);
 }
 
-IStreamPtr Manager::openCaseInsensitive(const std::string &name)
+IStreamPtr Manager::open(const std::string &name, bool &inGlobalBSA)
+{
+	return this->open(name.c_str(), inGlobalBSA);
+}
+
+IStreamPtr Manager::open(const std::string &name)
+{
+	bool dummy;
+	return this->open(name, dummy);
+}
+
+IStreamPtr Manager::openCaseInsensitive(const std::string &name, bool &inGlobalBSA)
 {
 	// Since the given filename is assumed to be unique in its directory, we only need to
 	// worry about filenames just like it but with different casing.
@@ -81,12 +101,10 @@ IStreamPtr Manager::openCaseInsensitive(const std::string &name)
 
 	// Case 1: upper first character, lower rest.
 	newName.front() = std::toupper(newName.front());
-	for (auto iter = newName.begin() + 1; iter != newName.end(); ++iter)
-	{
-		*iter = std::tolower(*iter);
-	}
+	std::for_each(newName.begin() + 1, newName.end(),
+		[](char &c) { c = std::tolower(c); });
 
-	IStreamPtr stream = this->open(newName);
+	IStreamPtr stream = this->open(newName, inGlobalBSA);
 
 	if (stream != nullptr)
 	{
@@ -100,83 +118,87 @@ IStreamPtr Manager::openCaseInsensitive(const std::string &name)
 			c = std::toupper(c);
 		}
 
-		stream = this->open(newName);
+		stream = this->open(newName, inGlobalBSA);
 
 		// The caller does error checking to see if this is null.
 		return stream;
 	}
 }
 
-bool Manager::exists(const char *name)
+IStreamPtr Manager::openCaseInsensitive(const std::string &name)
 {
-    std::ifstream file;
-    for(const std::string &path : gRootPaths)
-    {
-        file.open((path+name).c_str(), std::ios_base::binary);
-        if(file.is_open()) return true;
-    }
-
-    return gGlobalBsa.exists(name);
+	bool dummy;
+	return this->openCaseInsensitive(name, dummy);
 }
 
+bool Manager::exists(const char *name)
+{
+	std::ifstream file;
+	for (const std::string &path : gRootPaths)
+	{
+		file.open(path + name, std::ios_base::binary);
+		if (file.is_open()) return true;
+	}
+
+	return gGlobalBsa.exists(name);
+}
 
 void Manager::add_dir(const std::string &path, const std::string &pre, const char *pattern, std::vector<std::string> &names)
 {
-    DIR *dir = opendir(path.c_str());
-    if(!dir) return;
+	DIR *dir = opendir(path.c_str());
+	if (!dir) return;
 
-    dirent *ent;
-    while((ent=readdir(dir)) != nullptr)
-    {
-        if(strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)
-            continue;
+	dirent *ent;
+	while ((ent = readdir(dir)) != nullptr)
+	{
+		if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)
+			continue;
 
-        if(!S_ISDIR(ent->d_type))
-        {
-            std::string fname = pre + ent->d_name;
-            if(!pattern || fnmatch(pattern, fname.c_str(), 0) == 0)
-                names.push_back(fname);
-        }
-        else
-        {
-            std::string newpath = path+"/"+ent->d_name;
-            std::string newpre = pre+ent->d_name+"/";
-            add_dir(newpath, newpre, pattern, names);
-        }
-    }
+		if (!S_ISDIR(ent->d_type))
+		{
+			std::string fname = pre + ent->d_name;
+			if (!pattern || fnmatch(pattern, fname.c_str(), 0) == 0)
+				names.push_back(fname);
+		}
+		else
+		{
+			std::string newpath = path + "/" + ent->d_name;
+			std::string newpre = pre + ent->d_name + "/";
+			add_dir(newpath, newpre, pattern, names);
+		}
+	}
 
-    closedir(dir);
+	closedir(dir);
 }
 
 std::vector<std::string> Manager::list(const char *pattern) const
 {
-    std::vector<std::string> files;
+	std::vector<std::string> files;
 
-    auto piter = gRootPaths.rbegin();
-    while(piter != gRootPaths.rend())
-    {
-        add_dir(*piter+".", "", pattern, files);
-        ++piter;
-    }
+	auto piter = gRootPaths.rbegin();
+	while (piter != gRootPaths.rend())
+	{
+		add_dir(*piter + ".", "", pattern, files);
+		++piter;
+	}
 
-    if(!pattern)
-    {
-        const auto &list = gGlobalBsa.list();
-        std::copy(list.begin(), list.end(), std::back_inserter(files));
-    }
-    else
-    {
-        const auto &list = gGlobalBsa.list();
-        std::copy_if(list.begin(), list.end(), std::back_inserter(files),
-            [pattern](const std::string &name) -> bool
-            {
-                const char *dirsep = strrchr(name.c_str(), '/');
-                return (fnmatch(pattern, dirsep?dirsep:name.c_str(), 0) == 0);
-            }
-        );
-    }
+	if (!pattern)
+	{
+		const auto &list = gGlobalBsa.list();
+		std::copy(list.begin(), list.end(), std::back_inserter(files));
+	}
+	else
+	{
+		const auto &list = gGlobalBsa.list();
+		std::copy_if(list.begin(), list.end(), std::back_inserter(files),
+			[pattern](const std::string &name) -> bool
+		{
+			const char *dirsep = strrchr(name.c_str(), '/');
+			return (fnmatch(pattern, dirsep ? dirsep : name.c_str(), 0) == 0);
+		});
+	}
 
-    return files;
+	return files;
 }
 
 } // namespace VFS
--- components/vfs/manager.hpp
+++ components/vfs/manager.hpp
@@ -15,55 +15,54 @@ typedef std::shared_ptr<std::istream> IStreamPtr;
 
 inline uint32_t read_le32(std::istream &stream)
 {
-    char buf[4];
-    if(!stream.read(buf, sizeof(buf)) || stream.gcount() != sizeof(buf))
-        return 0;
-    return ((uint32_t(buf[0]    )&0x000000ff) | (uint32_t(buf[1]<< 8)&0x0000ff00) |
-            (uint32_t(buf[2]<<16)&0x00ff0000) | (uint32_t(buf[3]<<24)&0xff000000));
+	char buf[4];
+	if (!stream.read(buf, sizeof(buf)) || stream.gcount() != sizeof(buf))
+		return 0;
+	return ((uint32_t(buf[0]) & 0x000000ff) | (uint32_t(buf[1] << 8) & 0x0000ff00) |
+		(uint32_t(buf[2] << 16) & 0x00ff0000) | (uint32_t(buf[3] << 24) & 0xff000000));
 }
 
 inline uint16_t read_le16(std::istream &stream)
 {
-    char buf[2];
-    if(!stream.read(buf, sizeof(buf)) || stream.gcount() != sizeof(buf))
-        return 0;
-    return ((uint16_t(buf[0]   )&0x00ff) | (uint16_t(buf[1]<<8)&0xff00));
+	char buf[2];
+	if (!stream.read(buf, sizeof(buf)) || stream.gcount() != sizeof(buf))
+		return 0;
+	return ((uint16_t(buf[0]) & 0x00ff) | (uint16_t(buf[1] << 8) & 0xff00));
 }
 
 
 class Manager {
-    Manager(const Manager&) = delete;
-    Manager& operator=(const Manager&) = delete;
+	Manager(const Manager&) = delete;
+	Manager& operator=(const Manager&) = delete;
 
-    static void add_dir(const std::string &path, const std::string &pre, const char *pattern, std::vector<std::string> &names);
+	static void add_dir(const std::string &path, const std::string &pre, const char *pattern, std::vector<std::string> &names);
 
-    Manager();
+	Manager();
 
 public:
-    void initialize(std::string&& root_path=std::string());
-    void addDataPath(std::string&& path);
+	void initialize(std::string&& root_path = std::string());
+	void addDataPath(std::string&& path);
 
-    IStreamPtr open(const char *name);
-    IStreamPtr open(const std::string &name) { return open(name.c_str()); }
-    IStreamPtr open(std::string &&name) { return open(name.c_str()); }
+	IStreamPtr open(const char *name, bool &inGlobalBSA);
+	IStreamPtr open(const char *name);
+	IStreamPtr open(const std::string &name, bool &inGlobalBSA);
+	IStreamPtr open(const std::string &name);
 
 	// Special open method intended for Unix systems since the Arena floppy and CD versions don't
 	// have consistent casing for some files (like SPELLSG.65). This method is specific to Arena's
 	// files and is not a general solution for case-insensitive file loading.
-	// - For some reason, certain files are still found on Unix (like Arrows.cif) even when the
-	//   program requests different casing (like ARROWS.CIF), so this may be an issue of Unix
-	//   checking the filename without its extension when (supposedly) resolving ambiguities.
 	// - To do: replace with something using std::filesystem::equivalent() (C++17)?
+	IStreamPtr openCaseInsensitive(const std::string &name, bool &inGlobalBSA);
 	IStreamPtr openCaseInsensitive(const std::string &name);
 
-    bool exists(const char *name);
-    std::vector<std::string> list(const char *pattern=nullptr) const;
+	bool exists(const char *name);
+	std::vector<std::string> list(const char *pattern = nullptr) const;
 
-    static Manager &get()
-    {
-        static Manager manager;
-        return manager;
-    }
+	static Manager &get()
+	{
+		static Manager manager;
+		return manager;
+	}
 };
 
 } // namespace VFS
