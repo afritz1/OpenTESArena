9fe04692ac32afed34fc4624eeb4da962e6918ca
Allofich <rickywd@gmail.com>
2018-05-16 20:03:26 +0900

    Make Travis CI builds use latest WildMIDI code

--- CI/install.linux.sh
+++ CI/install.linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://github.com/Mindwerks/wildmidi/archive/wildmidi-0.4.0.tar.gz
-tar -xzvf wildmidi-0.4.0.tar.gz
-pushd wildmidi-wildmidi-0.4.0 && mkdir build && pushd build && cmake .. && make && sudo make install && popd && popd
+wget https://github.com/Mindwerks/wildmidi/archive/master.zip
+unzip master.zip
+pushd wildmidi-master && mkdir build && pushd build && cmake .. && make && sudo make install && popd && popd
