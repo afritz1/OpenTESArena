d3ea265327ed6beeb1692cb0dc9706562b801211
Aaron <afritz1@users.noreply.github.com>
2018-11-04 16:30:38 -0800

    Initial work on chunk definition.
    Each chunk in Arena is 64x64 blocks with a variable height depending on whether it's an interior or exterior. The original game probably doesn't store blocks above Y=2 for exteriors since they're all just duplicates of Y=2, but in this engine, every voxel will be uniquely addressable for each 3D coordinate in the chunk.

---
+++ OpenTESArena/src/World/Chunk.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+
+#include "Chunk.h"
+
+template <size_t T>
+Chunk<T>::Chunk()
+{
+	std::fill(this->voxels.begin(), this->voxels.end(), 0);
+}
+
+template <size_t T>
+constexpr int Chunk<T>::getWidth() const
+{
+	return WIDTH;
+}
+
+template <size_t T>
+constexpr int Chunk<T>::getHeight() const
+{
+	return HEIGHT;
+}
+
+template <size_t T>
+constexpr int Chunk<T>::getDepth() const
+{
+	return DEPTH;
+}
+
+template <size_t T>
+constexpr int Chunk<T>::getIndex(int x, int y, int z) const
+{
+	return x + (y * this->getWidth()) + (z * this->getWidth() * this->getHeight());
+}
+
+template <size_t T>
+uint16_t Chunk<T>::get(int x, int y, int z) const
+{
+	const int index = this->getIndex(x, y, z);
+	return this->voxels.at(index);
+}
+
+template<size_t T>
+void Chunk<T>::set(int x, int y, int z, uint16_t value)
+{
+	const int index = this->getIndex(x, y, z);
+	this->voxels.at(index) = value;
+}
---
+++ OpenTESArena/src/World/Chunk.h
@@ -0,0 +1,38 @@
+#ifndef CHUNK_H
+#define CHUNK_H
+
+#include <array>
+
+// A chunk is a 3D set of voxels for each part of Arena's world, for both interiors and exteriors.
+// It's a little odd that interiors are presented as chunks in the original game because it allows
+// the player to go into an expanse of nothingness/repeating data if they manage to bypass the
+// level perimeter.
+
+template <size_t T>
+class Chunk
+{
+private:
+	static constexpr int WIDTH = 64;
+	static constexpr int HEIGHT = T;
+	static constexpr int DEPTH = WIDTH;
+
+	// Indices into voxel data.
+	std::array<uint16_t, WIDTH * HEIGHT * DEPTH> voxels;
+
+	constexpr int getIndex(int x, int y, int z) const;
+public:
+	Chunk();
+
+	constexpr int getWidth() const;
+	constexpr int getHeight() const;
+	constexpr int getDepth() const;
+	uint16_t get(int x, int y, int z) const;
+	void set(int x, int y, int z, uint16_t value);
+};
+
+// Interior chunks are always three voxels high (ground, main floor, ceiling). Exteriors are
+// higher to allow for tall buildings.
+using InteriorChunk = Chunk<3>;
+using ExteriorChunk = Chunk<6>;
+
+#endif
