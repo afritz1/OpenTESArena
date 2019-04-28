1d43e30fb0be5977b32884dc4270739c4397be17
Aaron <afritz1@users.noreply.github.com>
2019-04-28 13:22:18 -0700

    Let Chunk be a base class.
    It was making things less easy trying to pass around a templated Chunk<T> type, so it will be a base class instead, only constructible through derived chunk classes. Efficiency will not be impacted because the get and set methods are non-virtual and ChunkSet already dynamically allocates chunks anyway.

--- OpenTESArena/src/World/Chunk.cpp
+++ OpenTESArena/src/World/Chunk.cpp
@@ -1,97 +1,106 @@
+#include <algorithm>
 #include <cassert>
 
 #include "Chunk.h"
 
-template <size_t T>
-Chunk<T>::Chunk(int x, int y)
+Chunk::Chunk(int x, int y, int height)
 {
-	this->voxels.fill(0);
+	// Set all voxels to air and unused.
+	const int voxelCount = Chunk::WIDTH * height * Chunk::DEPTH;
+	this->voxels = std::make_unique<VoxelID[]>(voxelCount);
+	std::fill(this->voxels.get(), this->voxels.get() + voxelCount, 0);
+
 	this->voxelData.fill(VoxelData());
+	this->activeVoxelData.fill(false);
 
 	// Let the first voxel data (air) be usable immediately. All default voxel IDs can safely point to it.
-	this->voxelDataCount = 1;
+	this->activeVoxelData.front() = true;
 
+	this->height = height;
 	this->x = x;
 	this->y = y;
 }
 
-template <size_t T>
-int Chunk<T>::getX() const
+int Chunk::getX() const
 {
 	return this->x;
 }
 
-template <size_t T>
-int Chunk<T>::getY() const
+int Chunk::getY() const
 {
 	return this->y;
 }
 
-template <size_t T>
-constexpr int Chunk<T>::getWidth() const
+constexpr int Chunk::getWidth() const
 {
-	return Chunk<T>::WIDTH;
+	return Chunk::WIDTH;
 }
 
-template <size_t T>
-constexpr int Chunk<T>::getHeight() const
+int Chunk::getHeight() const
 {
-	return Chunk<T>::HEIGHT;
+	return this->height;
 }
 
-template <size_t T>
-constexpr int Chunk<T>::getDepth() const
+constexpr int Chunk::getDepth() const
 {
-	return Chunk<T>::DEPTH;
+	return Chunk::DEPTH;
 }
 
-template <size_t T>
-constexpr bool Chunk<T>::coordIsValid(int x, int y, int z) const
+bool Chunk::coordIsValid(int x, int y, int z) const
 {
 	return (x >= 0) && (x < this->getWidth()) &&
 		(y >= 0) && (y < this->getHeight()) &&
 		(z >= 0) && (z < this->getDepth());
 }
 
-template <size_t T>
-constexpr int Chunk<T>::getIndex(int x, int y, int z) const
+int Chunk::getIndex(int x, int y, int z) const
 {
 	assert(this->coordIsValid(x, y, z));
 	return x + (y * this->getWidth()) + (z * this->getWidth() * this->getHeight());
 }
 
-template <size_t T>
-constexpr VoxelID Chunk<T>::get(int x, int y, int z) const
+VoxelID Chunk::get(int x, int y, int z) const
 {
 	const int index = this->getIndex(x, y, z);
 	return this->voxels[index];
 }
 
-template <size_t T>
-const VoxelData &Chunk<T>::getVoxelData(VoxelID id) const
+const VoxelData &Chunk::getVoxelData(VoxelID id) const
 {
 	assert(id < this->voxelData.size());
+	assert(this->activeVoxelData[id]);
 	return this->voxelData[id];
 }
 
-template <size_t T>
-constexpr void Chunk<T>::set(int x, int y, int z, VoxelID value)
+int Chunk::debug_getVoxelDataCount() const
+{
+	return static_cast<int>(
+		std::count(this->activeVoxelData.begin(), this->activeVoxelData.end(), true));
+}
+
+void Chunk::set(int x, int y, int z, VoxelID value)
 {
 	const int index = this->getIndex(x, y, z);
 	this->voxels[index] = value;
 }
 
-template <size_t T>
-VoxelID Chunk<T>::addVoxelData(VoxelData &&voxelData)
+VoxelID Chunk::addVoxelData(VoxelData &&voxelData)
 {
-	// If we ever reach more than 256 unique voxel datas, we need more bits per voxel.
-	assert(this->voxelDataCount < this->voxelData.size());
+	// Find a place to add the voxel data.
+	const auto iter = std::find(this->activeVoxelData.begin(), this->activeVoxelData.end(), false);
 
-	this->voxelData[this->voxelDataCount] = std::move(voxelData);
-	this->voxelDataCount++;
-	return this->voxelDataCount - 1;
+	// If we ever hit this, we need more bits per voxel.
+	assert(iter != this->activeVoxelData.end());
+
+	const VoxelID id = static_cast<VoxelID>(std::distance(this->activeVoxelData.begin(), iter));
+	this->voxelData[id] = std::move(voxelData);
+	this->activeVoxelData[id] = true;
+	return id;
 }
 
-// Template instantiations.
-template class Chunk<3>;
-template class Chunk<6>;
+void Chunk::removeVoxelData(VoxelID id)
+{
+	assert(id < this->voxelData.size());
+	this->voxelData[id] = VoxelData();
+	this->activeVoxelData[id] = false;
+}
--- OpenTESArena/src/World/Chunk.h
+++ OpenTESArena/src/World/Chunk.h
@@ -3,66 +3,78 @@
 
 #include <array>
 #include <cstdint>
+#include <memory>
 
 #include "VoxelData.h"
 
-// A chunk is a 3D set of voxels for each part of Arena's world, for both interiors and exteriors.
-// It's a little odd that interiors are presented as chunks in the original game because it allows
-// the player to go into an expanse of nothingness/repeating data if they manage to bypass the
-// level perimeter.
-
-// There should be fewer than 256 unique voxel types per chunk. In my original design with
-// voxel grids, each voxel was two bytes because a voxel grid was generally much larger than
-// a chunk, encapsulating several more voxel types. But within a single chunk, 256 should work.
+// There should be fewer than 256 unique voxel types per chunk. If we need more, then the data
+// can be redesigned to be something like 9 bits per voxel (and the ID type would be 16-bit).
 using VoxelID = uint8_t;
 
-template <size_t T>
+// A chunk is a 3D set of voxels for each part of the world, for both interiors and exteriors.
 class Chunk
 {
 private:
 	static constexpr int WIDTH = 64;
-	static constexpr int HEIGHT = T;
 	static constexpr int DEPTH = WIDTH;
+	static constexpr int MAX_VOXEL_DATA = 256;
 
 	// Indices into voxel data.
-	std::array<VoxelID, WIDTH * HEIGHT * DEPTH> voxels;
+	std::unique_ptr<VoxelID[]> voxels;
 
-	// Voxel data definitions, pointed to by voxel IDs.
-	std::array<VoxelData, 256> voxelData;
-	int voxelDataCount;
+	// Voxel data definitions, pointed to by voxel IDs. If the associated bool is true,
+	// the voxel data is in use by the voxel grid.
+	std::array<VoxelData, MAX_VOXEL_DATA> voxelData;
+	std::array<bool, MAX_VOXEL_DATA> activeVoxelData;
+
+	// Chunk height. Depends on whether it's an interior or exterior.
+	int height;
 
 	// Chunk coordinates.
 	int x, y;
-
-	constexpr bool coordIsValid(int x, int y, int z) const;
-	constexpr int getIndex(int x, int y, int z) const;
+protected:
+	Chunk(int x, int y, int height);
+private:
+	bool coordIsValid(int x, int y, int z) const;
+	int getIndex(int x, int y, int z) const;
 public:
-	Chunk(int x, int y);
-
 	int getX() const;
 	int getY() const;
 	constexpr int getWidth() const;
-	constexpr int getHeight() const;
+	int getHeight() const;
 	constexpr int getDepth() const;
 
 	// Gets the voxel ID at the given coordinate.
-	constexpr VoxelID get(int x, int y, int z) const;
+	VoxelID get(int x, int y, int z) const;
 
 	// Gets the voxel data associated with a voxel ID.
 	const VoxelData &getVoxelData(VoxelID id) const;
 
+	// Gets the number of active voxel data definitions.
+	int debug_getVoxelDataCount() const;
+
 	// Sets the voxel at the given coordinate.
-	constexpr void set(int x, int y, int z, VoxelID value);
+	void set(int x, int y, int z, VoxelID id);
 
 	// Adds a voxel data definition and returns its assigned ID.
 	VoxelID addVoxelData(VoxelData &&voxelData);
+
+	// Removes a voxel data definition so its corresponding voxel ID can be reused.
+	void removeVoxelData(VoxelID id);
 };
 
-// Template instantiations at end of .cpp file.
+// Interior chunks are always three voxels high (ground, main floor, ceiling).
+class InteriorChunk final : public Chunk
+{
+public:
+	InteriorChunk(int x, int y) : Chunk(x, y, 3) { }
+};
 
-// Interior chunks are always three voxels high (ground, main floor, ceiling). Exteriors are
-// higher to allow for tall buildings.
-using InteriorChunk = Chunk<3>;
-using ExteriorChunk = Chunk<6>;
+// Exteriors are higher to allow for tall buildings.
+class ExteriorChunk final : public Chunk
+{
+public:
+	ExteriorChunk(int x, int y) : Chunk(x, y, 6) { }
+};
 
 #endif
