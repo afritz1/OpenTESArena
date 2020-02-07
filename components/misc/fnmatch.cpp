2766a1274f12538b1f0ac1a12ab2e8b1221695c3
Aaron <afritz1@users.noreply.github.com>
2020-02-06 19:19:12 -0800

    Switched to regex_match implementation in fnmatch().
    This replaces the stripped-down GNU version. Seems to work just fine -- it's Manager::list() that is producing very incorrect directory listings though, but that function isn't used anywhere anyway, so it can be fixed separately.

--- components/misc/fnmatch.cpp
+++ components/misc/fnmatch.cpp
@@ -1,5 +1,23 @@
+#include <regex>
+
 #include "fnmatch.h"
 #include "../debug/Debug.h"
+#include "../utilities/String.h"
+
+namespace
+{
+	// Converts fnmatch pattern to ECMAScript regex format.
+	std::string PatternToECMA(const char *pattern)
+	{
+		std::string translated = pattern;
+		translated = String::replace(translated, "\\", "\\\\");
+		translated = String::replace(translated, ".", "\\.");
+		translated = String::replace(translated, "?", "[^/]");
+		translated = String::replace(translated, "*", "[^/]*");
+		translated = String::replace(translated, "[!", "[^");
+		return translated;
+	}
+}
 
 // Only use this on the implementation side because it's not part of the official interface.
 #undef FNM_SUCCESS
@@ -23,167 +41,7 @@ int fnmatch(const char *pattern, const char *str, int flags)
 
 	static_cast<void>(flags);
 
-	// Iterator through pattern string.
-	const char *patIter = pattern;
-
-	// Iterator through match string.
-	const char *strIter = str;
-
-	// Value of currently selected pattern string character.
-	char curPatChar;
-
-	while ((curPatChar = *patIter++) != '\0')
-	{
-		switch (curPatChar)
-		{
-		case '?':
-			if (*strIter == '\0')
-			{
-				return FNM_NOMATCH;
-			}
-
-			break;
-
-		case '\\':
-			curPatChar = *patIter++;
-			if (*strIter != curPatChar)
-			{
-				return FNM_NOMATCH;
-			}
-
-			break;
-
-		case '*':
-		{
-			for (curPatChar = *patIter++; (curPatChar == '?') || (curPatChar == '*'); curPatChar = *patIter++, strIter++)
-			{
-				if ((curPatChar == '?') && (*strIter == '\0'))
-				{
-					return FNM_NOMATCH;
-				}
-			}
-
-			if (curPatChar == '\0')
-			{
-				return FNM_SUCCESS;
-			}
-
-			const char maybeEscapedChar = (curPatChar == '\\') ? *patIter : curPatChar;
-
-			for (patIter--; *strIter != '\0'; strIter++)
-			{
-				if ((curPatChar == '[') || (*strIter == maybeEscapedChar))
-				{
-					return FNM_SUCCESS;
-				}
-			}
-
-			return FNM_NOMATCH;
-		}
-
-		case '[':
-		{
-			if (*strIter == '\0')
-			{
-				return FNM_NOMATCH;
-			}
-
-			// Nonzero if the sense of the character class is inverted.
-			const bool inverted = (*patIter == '!') || (*patIter == '^');
-			if (inverted)
-			{
-				patIter++;
-			}
-
-			curPatChar = *patIter++;
-			while (true)
-			{
-				char cStart = curPatChar;
-				char cEnd = curPatChar;
-
-				if (curPatChar == '\\')
-				{
-					cEnd = *patIter++;
-					cStart = cEnd;
-				}
-
-				if (curPatChar == '\0')
-				{
-					// [ (unterminated) loses.
-					return FNM_NOMATCH;
-				}
-
-				curPatChar = *patIter++;
-
-				if ((curPatChar == '-') && (*patIter != ']'))
-				{
-					cEnd = *patIter++;
-					if (cEnd == '\\')
-					{
-						cEnd = *patIter++;
-					}
-
-					if (cEnd == '\0')
-					{
-						return FNM_NOMATCH;
-					}
-
-					curPatChar = *patIter++;
-				}
-
-				if ((*strIter >= cStart) && (*strIter <= cEnd))
-				{
-					goto matchedLeftBracket;
-				}
-
-				if (curPatChar == ']')
-				{
-					break;
-				}
-			}
-
-			if (!inverted)
-			{
-				return FNM_NOMATCH;
-			}
-
-			break;
-
-		matchedLeftBracket:
-			// Skip the rest of the [...] that already matched.
-			while (curPatChar != ']')
-			{
-				if (curPatChar == '\0')
-				{
-					// [... (unterminated) loses.
-					return FNM_NOMATCH;
-				}
-
-				curPatChar = *patIter++;
-				if (curPatChar == '\\')
-				{
-					// 1003.2d11 is unclear if this is right.
-					patIter++;
-				}
-			}
-
-			if (inverted)
-			{
-				return FNM_NOMATCH;
-			}
-
-			break;
-		}
-
-		default:
-			if (curPatChar != *strIter)
-			{
-				return FNM_NOMATCH;
-			}
-		}
-
-		strIter++;
-	}
-
-	return (*strIter == '\0') ? FNM_SUCCESS : FNM_NOMATCH;
+	const std::string patternECMAString = PatternToECMA(pattern);
+	const std::regex patternECMA(patternECMAString, std::regex_constants::icase);
+	return std::regex_match(str, patternECMA) ? FNM_SUCCESS : FNM_NOMATCH;
 }
