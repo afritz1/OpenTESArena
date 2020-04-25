d6017f0f95737d5ef91065119977cf237757b44b
Aaron <afritz1@users.noreply.github.com>
2020-04-25 13:40:03 -0700

    Added DOSUtils.h.

---
+++ components/dos/DOSUtils.h
@@ -0,0 +1,10 @@
+#ifndef DOS_UTILS_H
+#define DOS_UTILS_H
+
+namespace DOSUtils
+{
+	// DOS filename buffer size (8 characters + 1 period + 3 extension + 1 null).
+	constexpr int FILENAME_BUFFER_SIZE = 13;
+}
+
+#endif
