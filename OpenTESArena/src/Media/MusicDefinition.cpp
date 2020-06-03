6861dc056f523a4f5bcd31e588030621e0cae66e
Aaron <afritz1@users.noreply.github.com>
2020-06-02 21:50:48 -0700

    Added MusicDefinition.
    Will be used with some music library class that holds collections of all music definitions and allows for randomly selecting music based on some arguments. The music definitions could in theory be moved out to a text file so all music filenames in the game would be moddable.

---
+++ OpenTESArena/src/Media/MusicDefinition.cpp
@@ -0,0 +1,115 @@
+#include "MusicDefinition.h"
+
+#include "components/debug/Debug.h"
+
+void MusicDefinition::CinematicMusicDefinition::init(CinematicMusicDefinition::Type type)
+{
+	this->type = type;
+}
+
+void MusicDefinition::InteriorMusicDefinition::init(InteriorMusicDefinition::Type type)
+{
+	this->type = type;
+}
+
+void MusicDefinition::JingleMusicDefinition::init(LocationDefinition::CityDefinition::Type cityType,
+	ClimateType climateType)
+{
+	this->cityType = cityType;
+	this->climateType = climateType;
+}
+
+void MusicDefinition::WeatherMusicDefinition::init(WeatherType type)
+{
+	this->type = type;
+}
+
+void MusicDefinition::init(std::string &&filename, Type type)
+{
+	this->filename = std::move(filename);
+	this->type = type;
+}
+
+void MusicDefinition::initCharacterCreation(std::string &&filename)
+{
+	this->init(std::move(filename), Type::CharacterCreation);
+}
+
+void MusicDefinition::initCinematic(std::string &&filename, CinematicMusicDefinition::Type type)
+{
+	this->init(std::move(filename), Type::Cinematic);
+	this->cinematic.init(type);
+}
+
+void MusicDefinition::initDungeon(std::string &&filename)
+{
+	this->init(std::move(filename), Type::Dungeon);
+}
+
+void MusicDefinition::initInterior(std::string &&filename, InteriorMusicDefinition::Type type)
+{
+	this->init(std::move(filename), Type::Interior);
+	this->interior.init(type);
+}
+
+void MusicDefinition::initJingle(std::string &&filename,
+	LocationDefinition::CityDefinition::Type cityType, ClimateType climateType)
+{
+	this->init(std::move(filename), Type::Jingle);
+	this->jingle.init(cityType, climateType);
+}
+
+void MusicDefinition::initMainMenu(std::string &&filename)
+{
+	this->init(std::move(filename), Type::MainMenu);
+}
+
+void MusicDefinition::initNight(std::string &&filename)
+{
+	this->init(std::move(filename), Type::Night);
+}
+
+void MusicDefinition::initSwimming(std::string &&filename)
+{
+	this->init(std::move(filename), Type::Swimming);
+}
+
+void MusicDefinition::initWeather(std::string &&filename, WeatherType type)
+{
+	this->init(std::move(filename), Type::Weather);
+	this->weather.init(type);
+}
+
+const std::string &MusicDefinition::getFilename() const
+{
+	return this->filename;
+}
+
+MusicDefinition::Type MusicDefinition::getType() const
+{
+	return this->type;
+}
+
+const MusicDefinition::CinematicMusicDefinition &MusicDefinition::getCinematicMusicDefinition() const
+{
+	DebugAssert(this->type == Type::Cinematic);
+	return this->cinematic;
+}
+
+const MusicDefinition::InteriorMusicDefinition &MusicDefinition::getInteriorMusicDefinition() const
+{
+	DebugAssert(this->type == Type::Interior);
+	return this->interior;
+}
+
+const MusicDefinition::JingleMusicDefinition &MusicDefinition::getJingleMusicDefinition() const
+{
+	DebugAssert(this->type == Type::Jingle);
+	return this->jingle;
+}
+
+const MusicDefinition::WeatherMusicDefinition &MusicDefinition::getWeatherMusicDefinition() const
+{
+	DebugAssert(this->type == Type::Weather);
+	return this->weather;
+}
---
+++ OpenTESArena/src/Media/MusicDefinition.h
@@ -0,0 +1,107 @@
+#ifndef MUSIC_DEFINITION_H
+#define MUSIC_DEFINITION_H
+
+#include <string>
+
+#include "../World/LocationDefinition.h"
+
+enum class ClimateType;
+enum class WeatherType;
+
+class MusicDefinition
+{
+public:
+	enum class Type
+	{
+		CharacterCreation,
+		Cinematic,
+		Dungeon,
+		Interior,
+		Jingle,
+		MainMenu,
+		Night,
+		Swimming,
+		Weather
+	};
+private:
+	struct CinematicMusicDefinition
+	{
+		enum class Type
+		{
+			Intro,
+			DreamGood,
+			DreamBad,
+			Ending
+		};
+
+		CinematicMusicDefinition::Type type;
+
+		void init(CinematicMusicDefinition::Type type);
+	};
+
+	struct InteriorMusicDefinition
+	{
+		enum class Type
+		{
+			House,
+			MagesGuild,
+			Palace,
+			Shop,
+			Tavern,
+			Temple
+		};
+
+		InteriorMusicDefinition::Type type;
+
+		void init(InteriorMusicDefinition::Type type);
+	};
+
+	struct JingleMusicDefinition
+	{
+		LocationDefinition::CityDefinition::Type cityType;
+		ClimateType climateType;
+
+		void init(LocationDefinition::CityDefinition::Type cityType, ClimateType climateType);
+	};
+
+	struct WeatherMusicDefinition
+	{
+		WeatherType type;
+
+		void init(WeatherType type);
+	};
+
+	std::string filename;
+	Type type;
+
+	union
+	{
+		CinematicMusicDefinition cinematic;
+		InteriorMusicDefinition interior;
+		JingleMusicDefinition jingle;
+		WeatherMusicDefinition weather;
+	};
+
+	void init(std::string &&filename, Type type);
+public:
+	void initCharacterCreation(std::string &&filename);
+	void initCinematic(std::string &&filename, CinematicMusicDefinition::Type type);
+	void initDungeon(std::string &&filename);
+	void initInterior(std::string &&filename, InteriorMusicDefinition::Type type);
+	void initJingle(std::string &&filename, LocationDefinition::CityDefinition::Type cityType,
+		ClimateType climateType);
+	void initMainMenu(std::string &&filename);
+	void initNight(std::string &&filename);
+	void initSwimming(std::string &&filename);
+	void initWeather(std::string &&filename, WeatherType type);
+
+	const std::string &getFilename() const;
+	Type getType() const;
+
+	const CinematicMusicDefinition &getCinematicMusicDefinition() const;
+	const InteriorMusicDefinition &getInteriorMusicDefinition() const;
+	const JingleMusicDefinition &getJingleMusicDefinition() const;
+	const WeatherMusicDefinition &getWeatherMusicDefinition() const;
+};
+
+#endif
