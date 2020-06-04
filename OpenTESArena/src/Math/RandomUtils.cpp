ba5d11969ebfc4aa8dc8f4e5358707ee2bb8bed9
Aaron <afritz1@users.noreply.github.com>
2020-06-03 23:38:51 -0700

    Added another Random shuffle function.

--- OpenTESArena/src/Math/RandomUtils.cpp
+++ OpenTESArena/src/Math/RandomUtils.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <random>
 
+#include "Random.h"
 #include "RandomUtils.h"
 
 #include "components/debug/Debug.h"
@@ -13,4 +14,20 @@ void RandomUtils::shuffle(int *buffer, int count)
 	int *bufferEnd = bufferBegin + count;
 	std::random_device randomDevice;
 	std::shuffle(bufferBegin, bufferEnd, std::default_random_engine(randomDevice()));
+}
+
+void RandomUtils::shuffle(int *buffer, int count, Random &random)
+{
+	DebugAssert(buffer != nullptr);
+
+	for (int i = 0; i < count - 1; i++)
+	{
+		const int swapIndex = i + random.next(count - i);
+		if (swapIndex != i)
+		{
+			const int temp = buffer[swapIndex];
+			buffer[swapIndex] = buffer[i];
+			buffer[i] = temp;
+		}
+	}
 }
--- OpenTESArena/src/Math/RandomUtils.h
+++ OpenTESArena/src/Math/RandomUtils.h
@@ -1,10 +1,15 @@
 #ifndef RANDOM_UTILS_H
 #define RANDOM_UTILS_H
 
+class Random;
+
 namespace RandomUtils
 {
 	// Shuffles the elements randomly with an unspecified RNG source.
 	void shuffle(int *buffer, int count);
+
+	// Shuffles the elements randomly with the given RNG source.
+	void shuffle(int *buffer, int count, Random &random);
 }
 
 #endif
