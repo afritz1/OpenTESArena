a0a2683d8083813c0f7815740171428683f1dcb5
Aaron <afritz1@users.noreply.github.com>
2021-01-09 11:51:56 -0800

    Revised RendererInterface for new texture ID types.

--- OpenTESArena/src/Rendering/RendererInterface.h
+++ OpenTESArena/src/Rendering/RendererInterface.h
@@ -1,12 +1,14 @@
 #ifndef RENDERER_INTERFACE_H
 #define RENDERER_INTERFACE_H
 
-#include "RenderCamera.h"
-#include "RenderDefinitionGroup.h"
-#include "RenderFrameSettings.h"
-#include "RenderInitSettings.h"
-#include "RenderInstanceGroup.h"
 #include "RenderTextureUtils.h"
+
+class RenderCamera;
+class RenderDefinitionGroup;
+class RenderFrameSettings;
+class RenderInitSettings;
+class RenderInstanceGroup;
+class TextureBuilder;
 
 class RendererInterface
 {
@@ -14,10 +16,14 @@ public:
 	virtual void init(const RenderInitSettings &settings) = 0;
 	virtual void shutdown() = 0;
 	virtual void resize(int width, int height) = 0;
-	virtual VoxelTextureID createVoxelTexture(int width, int height) = 0; // @todo: texture builder
-	virtual SpriteTextureID createSpriteTexture(int width, int height) = 0; // @todo: texture builder
-	virtual void freeVoxelTexture(VoxelTextureID textureID) = 0;
-	virtual void freeSpriteTexture(SpriteTextureID textureID) = 0;
+	virtual VoxelTextureID createVoxelTexture(const TextureBuilder &textureBuilder) = 0;
+	virtual EntityTextureID createEntityTexture(const TextureBuilder &textureBuilder) = 0;
+	virtual SkyTextureID createSkyTexture(const TextureBuilder &textureBuilder) = 0;
+	virtual UiTextureID createUiTexture(const TextureBuilder &textureBuilder) = 0;
+	virtual void freeVoxelTexture(VoxelTextureID id) = 0;
+	virtual void freeEntityTexture(EntityTextureID id) = 0;
+	virtual void freeSkyTexture(SkyTextureID id) = 0;
+	virtual void freeUiTexture(UiTextureID id) = 0;
 	virtual void submitFrame(const RenderDefinitionGroup &defGroup, const RenderInstanceGroup &instGroup,
 		const RenderCamera &camera, const RenderFrameSettings &settings) = 0;
 	virtual void present() = 0;
