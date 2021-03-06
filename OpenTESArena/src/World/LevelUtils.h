0d1d3239fff7740f66edc7dd88c81df9b489f6b3
Aaron <afritz1@users.noreply.github.com>
2021-03-06 14:59:17 -0800

    Removed obsolete LevelUtils function.

--- OpenTESArena/src/World/LevelUtils.h
+++ OpenTESArena/src/World/LevelUtils.h
@@ -1,18 +1,9 @@
 #ifndef LEVEL_UTILS_H
 #define LEVEL_UTILS_H
 
-#include "Coord.h"
-
-class LevelDefinition;
-class LevelInfoDefinition;
-class TransitionDefinition;
-
 namespace LevelUtils
 {
-	// Tries to get the first transition at the given level coordinate. This is only needed since chunks don't
-	// store transition data yet.
-	const TransitionDefinition *tryGetTransition(const LevelInt3 &voxel, const LevelDefinition &levelDef,
-		const LevelInfoDefinition &levelInfoDef);
+	
 }
 
 #endif
