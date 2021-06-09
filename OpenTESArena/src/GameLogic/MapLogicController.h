6866928aa5e56524e937c92d04fc3d16c7de0dfb
Aaron <afritz1@users.noreply.github.com>
2021-06-08 19:19:05 -0700

    Added GameLogic folder and logic controller stubs.

---
+++ OpenTESArena/src/GameLogic/MapLogicController.h
@@ -0,0 +1,9 @@
+#ifndef MAP_LOGIC_CONTROLLER_H
+#define MAP_LOGIC_CONTROLLER_H
+
+namespace MapLogicController
+{
+
+}
+
+#endif
---
+++ OpenTESArena/src/GameLogic/PlayerLogicController.h
@@ -0,0 +1,9 @@
+#ifndef PLAYER_LOGIC_CONTROLLER_H
+#define PLAYER_LOGIC_CONTROLLER_H
+
+namespace PlayerLogicController
+{
+
+}
+
+#endif
