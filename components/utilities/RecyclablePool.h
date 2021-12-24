e394bacff81b486bbe8d552b7be2c58648650759
Aaron <afritz1@users.noreply.github.com>
2021-12-23 16:20:39 -0800

    Added RecyclablePool.

---
+++ components/utilities/RecyclablePool.h
@@ -0,0 +1,104 @@
+#ifndef RECYCLABLE_POOL_H
+#define RECYCLABLE_POOL_H
+
+#include <algorithm>
+#include <deque>
+#include <type_traits>
+#include <vector>
+
+#include "../debug/Debug.h"
+
+// Contiguous pool that allows elements to be freed and their position reused by future elements
+// without affecting other elements.
+
+template <typename ElementT, typename IdT>
+class RecyclablePool
+{
+private:
+	static_assert(std::is_default_constructible_v<ElementT>);
+	static_assert(std::is_move_assignable_v<ElementT>);
+	static_assert(std::is_standard_layout_v<ElementT>);
+	static_assert(std::is_integral_v<IdT>);
+	
+	std::vector<ElementT> elements;
+	std::deque<IdT> freedIDs;
+	IdT nextID;
+
+	bool isFreedID(IdT id) const
+	{
+		return std::find(this->freedIDs.begin(), this->freedIDs.end(), id) != this->freedIDs.end();
+	}
+
+	bool isValidID(IdT id) const
+	{
+		if ((id < 0) || (id >= this->nextID))
+		{
+			return false;
+		}
+
+		if (this->isFreedID(id))
+		{
+			return false;
+		}
+
+		return true;
+	}
+public:
+	RecyclablePool()
+	{
+		this->nextID = 0;
+	}
+
+	ElementT &get(IdT id)
+	{
+		DebugAssert(this->isValidID(id));
+		DebugAssertIndex(this->elements, id);
+		return this->elements[id];
+	}
+
+	const ElementT &get(IdT id) const
+	{
+		DebugAssert(this->isValidID(id));
+		DebugAssertIndex(this->elements, id);
+		return this->elements[id];
+	}
+
+	bool tryAlloc(IdT *outID)
+	{
+		if (!this->freedIDs.empty())
+		{
+			*outID = this->freedIDs.front();
+			this->freedIDs.pop_front();
+		}
+		else
+		{
+			*outID = this->nextID;
+			this->nextID++;
+			this->elements.emplace_back(ElementT());
+		}
+
+		return true;
+	}
+
+	void free(IdT id)
+	{
+		if (!this->isValidID(id))
+		{
+			DebugCrash("Invalid ID to free: \"" + std::to_string(id) + "\"");
+		}
+
+		this->freedIDs.emplace_back(id);
+		
+		DebugAssertIndex(this->elements, id);
+		this->elements[id] = ElementT();
+	}
+
+	void clear()
+	{
+		this->elements.clear();
+		this->freedIDs.clear();
+		this->nextID = 0;
+	}
+};
+
+#endif
