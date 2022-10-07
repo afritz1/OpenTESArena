214998a15232b8535be0c72a69ffa6b676344c02
Aaron <afritz1@users.noreply.github.com>
2022-10-06 19:11:45 -0700

    Deleted obsolete Travis CI files.

--- CI/before_install.linux.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install libegl1-mesa-dev libgles2-mesa-dev libsdl2-dev libopenal-dev
--- CI/install.linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/Mindwerks/wildmidi/archive/master.zip
-unzip master.zip
-pushd wildmidi-master && mkdir build && pushd build && cmake .. && make && sudo make install && popd && popd
