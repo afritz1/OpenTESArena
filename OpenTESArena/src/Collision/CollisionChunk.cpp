ea3fcef5a850af93ec5b1d1ba75d8844536cf826
Aaron <afritz1@users.noreply.github.com>
2022-12-23 07:06:11 -0800

    Added CollisionChunk functions.

--- OpenTESArena/src/Collision/CollisionChunk.cpp
+++ OpenTESArena/src/Collision/CollisionChunk.cpp
@@ -1,12 +1,55 @@
 #include "CollisionChunk.h"
 
+#include "components/debug/Debug.h"
+
 void CollisionChunk::init(const ChunkInt2 &position, int height)
 {
 	Chunk::init(position, height);
-	
+
+	// Let the first definition (air) be usable immediately. All default IDs can safely point to it.
+	this->meshDefs.emplace_back(CollisionMeshDefinition());
+
 	this->meshDefIDs.init(Chunk::WIDTH, height, Chunk::DEPTH);
 	this->meshDefIDs.fill(CollisionChunk::AIR_COLLISION_MESH_DEF_ID);
 
 	this->enabledColliders.init(Chunk::WIDTH, height, Chunk::DEPTH);
 	this->enabledColliders.fill(false);
+}
+
+int CollisionChunk::getCollisionMeshDefCount() const
+{
+	return static_cast<int>(this->meshDefs.size());
+}
+
+const CollisionMeshDefinition &CollisionChunk::getCollisionMeshDef(CollisionMeshDefID id) const
+{
+	DebugAssertIndex(this->meshDefs, id);
+	return this->meshDefs[id];
+}
+
+CollisionChunk::CollisionMeshDefID CollisionChunk::getCollisionMeshDefID(SNInt x, int y, WEInt z) const
+{
+	return this->meshDefIDs.get(x, y, z);
+}
+
+void CollisionChunk::setCollisionMeshDefID(SNInt x, int y, WEInt z, CollisionMeshDefID id)
+{
+	this->meshDefIDs.set(x, y, z, id);
+}
+
+bool CollisionChunk::isColliderEnabled(SNInt x, int y, WEInt z) const
+{
+	return this->enabledColliders.get(x, y, z);
+}
+
+void CollisionChunk::setColliderEnabled(SNInt x, int y, WEInt z, bool enabled)
+{
+	this->enabledColliders.set(x, y, z, enabled);
+}
+
+CollisionChunk::CollisionMeshDefID CollisionChunk::addCollisionMeshDef(CollisionMeshDefinition &&meshDef)
+{
+	const CollisionMeshDefID id = static_cast<CollisionMeshDefID>(this->meshDefs.size());
+	this->meshDefs.emplace_back(std::move(meshDef));
+	return id;
 }
--- OpenTESArena/src/Collision/CollisionChunk.h
+++ OpenTESArena/src/Collision/CollisionChunk.h
@@ -13,12 +13,24 @@ class CollisionChunk final : public Chunk
 public:
 	using CollisionMeshDefID = int;
 private:
+	std::vector<CollisionMeshDefinition> meshDefs;
 	Buffer3D<CollisionMeshDefID> meshDefIDs;
 	Buffer3D<bool> enabledColliders;
 public:
 	static constexpr CollisionMeshDefID AIR_COLLISION_MESH_DEF_ID = 0;
 
 	void init(const ChunkInt2 &position, int height);
+
+	int getCollisionMeshDefCount() const;
+	const CollisionMeshDefinition &getCollisionMeshDef(CollisionMeshDefID id) const;
+
+	CollisionMeshDefID getCollisionMeshDefID(SNInt x, int y, WEInt z) const;
+	void setCollisionMeshDefID(SNInt x, int y, WEInt z, CollisionMeshDefID id);
+
+	bool isColliderEnabled(SNInt x, int y, WEInt z) const;
+	void setColliderEnabled(SNInt x, int y, WEInt z, bool enabled);
+
+	CollisionMeshDefID addCollisionMeshDef(CollisionMeshDefinition &&meshDef);
 };
 
 #endif
