2b90cede00c921588647d5de24758bca62d62d0f
Aaron <afritz1@users.noreply.github.com>
2024-01-21 09:27:41 -0800

    Fixed light insertion by distance.

--- OpenTESArena/src/Rendering/RenderLightUtils.cpp
+++ OpenTESArena/src/Rendering/RenderLightUtils.cpp
@@ -25,42 +25,71 @@ void RenderLightIdList::tryAddLight(RenderLightID id, double distanceSqr)
 {
 	DebugAssert(distanceSqr >= 0.0);
 
-	if (this->lightCount >= static_cast<int>(std::size(this->lightIDs)))
-	{
-		return;
-	}
-
 	const auto beginIter = std::begin(this->lightIDs);
 	const auto endIter = beginIter + this->lightCount;
 	const auto existsIter = std::find(beginIter, endIter, id);
 	if (existsIter != endIter)
 	{
 		DebugLogWarning("Light ID " + std::to_string(id) + " already in list.");
+
+		const int existsIndex = static_cast<int>(std::distance(beginIter, existsIter));
+		if (distanceSqr != this->distanceSqrs[existsIndex])
+		{
+			DebugLogWarning("Light ID " + std::to_string(id) + " distance changed, it should be reordered.");
+		}
+
 		return;
 	}
 
-	// Find insertion index so IDs remain sorted.
-	int insertIndex = 0;
-	for (int i = 0; i < this->lightCount; i++)
+	int insertIndex = -1;
+	for (int i = 0; i < MAX_LIGHTS; i++)
 	{
-		const double curDistanceSqr = this->distanceSqrs[i];
-		if (distanceSqr < curDistanceSqr)
+		if (i == this->lightCount)
 		{
 			insertIndex = i;
 			break;
 		}
+		else if (i < this->lightCount)
+		{
+			const double curDistanceSqr = this->distanceSqrs[i];
+			if (distanceSqr < curDistanceSqr)
+			{
+				insertIndex = i;
+				break;
+			}
+		}
 	}
 
-	// Shift elements back.
-	for (int i = this->lightCount; i > insertIndex; i--)
+	if (insertIndex == -1)
 	{
-		this->lightIDs[i] = this->lightIDs[i - 1];
-		this->distanceSqrs[i] = this->distanceSqrs[i - 1];
+		// No room for this light.
+		return;
 	}
 
-	this->lightIDs[insertIndex] = id;
-	this->distanceSqrs[insertIndex] = distanceSqr;
-	this->lightCount++;
+	const bool isInsert = this->lightCount < MAX_LIGHTS;
+	if (isInsert)
+	{
+		for (int i = this->lightCount; i > insertIndex; i--)
+		{
+			this->lightIDs[i] = this->lightIDs[i - 1];
+			this->distanceSqrs[i] = this->distanceSqrs[i - 1];
+		}
+
+		this->lightIDs[insertIndex] = id;
+		this->distanceSqrs[insertIndex] = distanceSqr;
+		this->lightCount++;
+	}
+	else
+	{
+		for (int i = this->lightCount - 1; i > insertIndex; i--)
+		{
+			this->lightIDs[i] = this->lightIDs[i - 1];
+			this->distanceSqrs[i] = this->distanceSqrs[i - 1];
+		}
+
+		this->lightIDs[insertIndex] = id;
+		this->distanceSqrs[insertIndex] = distanceSqr;
+	}
 }
 
 void RenderLightIdList::removeLightAt(int index)
