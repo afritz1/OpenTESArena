a46f27ff391fb86deb4fe32ca605415d92f6d725
Aaron <afritz1@users.noreply.github.com>
2024-08-31 18:07:44 -0700

    Added CollisionShapeDefinition.
    Tied to the gameplay side of voxels and entities. Jolt uses them to instantiate its own bodies.

---
+++ OpenTESArena/src/Collision/CollisionShapeDefinition.h
@@ -0,0 +1,46 @@
+#ifndef COLLISION_SHAPE_DEFINITION_H
+#define COLLISION_SHAPE_DEFINITION_H
+
+#include "../Math/MathUtils.h"
+
+enum class CollisionShapeType
+{
+	Box, // Voxels
+	Capsule // Entities inc. projectiles
+};
+
+struct CollisionBoxShapeDefinition
+{
+	double width, height, depth;
+	double yOffset; // Elevation above bottom of voxel.
+	Radians yRotation; // For diagonal walls.
+
+	void init(double width, double height, double depth, double yOffset, Radians yRotation);
+};
+
+struct CollisionCapsuleShapeDefinition
+{
+	double radius, middleHeight;
+	double totalHeight; // (radius * 2) + middleHeight
+
+	void init(double radius, double middleHeight);
+};
+
+struct CollisionShapeDefinition
+{
+	CollisionShapeType type;
+
+	union
+	{
+		CollisionBoxShapeDefinition box;
+		CollisionCapsuleShapeDefinition capsule;
+	};
+
+	CollisionShapeDefinition();
+
+	void initBox(double width, double height, double depth, double yOffset, Radians yRotation);
+	void initCapsule(double radius, double middleHeight);
+	void initCapsuleAsSphere(double radius);
+};
+
+#endif
