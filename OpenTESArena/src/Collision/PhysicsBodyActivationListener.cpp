4401e6b758b0de298ff78b2f3097020008dc93a5
Aaron <afritz1@users.noreply.github.com>
2024-11-03 15:22:42 -0800

    Turned off physics log spam.

--- OpenTESArena/src/Collision/PhysicsBodyActivationListener.cpp
+++ OpenTESArena/src/Collision/PhysicsBodyActivationListener.cpp
@@ -7,10 +7,10 @@
 
 void PhysicsBodyActivationListener::OnBodyActivated(const JPH::BodyID &bodyID, uint64_t bodyUserData)
 {
-	DebugLog("Body " + std::to_string(bodyID.GetIndex()) + " got activated.");
+	//DebugLog("Body " + std::to_string(bodyID.GetIndex()) + " got activated.");
 }
 
 void PhysicsBodyActivationListener::OnBodyDeactivated(const JPH::BodyID &bodyID, uint64_t bodyUserData)
 {
-	DebugLog("Body " + std::to_string(bodyID.GetIndex()) + " went to sleep.");
+	//DebugLog("Body " + std::to_string(bodyID.GetIndex()) + " went to sleep.");
 }
