2f809143e22a31cf2167e0336283c04c9529879c
Aaron <afritz1@users.noreply.github.com>
2024-11-03 09:53:40 -0800

    Added some player movement todos.

--- OpenTESArena/src/GameLogic/PlayerLogicController.h
+++ OpenTESArena/src/GameLogic/PlayerLogicController.h
@@ -16,8 +16,17 @@ namespace PlayerLogicController
 	Double2 makeTurningAngularValues(Game &game, double dt, BufferView<const Rect> nativeCursorRegions);
 
 
+	// @todo: can't there just be a MakeNextVelocity() convenience function that 
+	// determines where the player is headed next? or a GetAccelerationFromInput()?
+
+	// @todo: change these void functions to ones that return a value (acceleration vector, immediate acceleration)
+
 	// Handles input for player movement in the game world.
 	void handlePlayerMovement(Game &game, double dt, BufferView<const Rect> nativeCursorRegions);
+
+
+	// @todo: change this to be framed like a physics ray cast: it generates a result of what was tested against,
+	// then afterwards we decide to play a hit sound or miss sound, etc.
 
 	// Handles input for the player's attack. Takes the change in mouse position since the previous frame.
 	void handlePlayerAttack(Game &game, const Int2 &mouseDelta);
