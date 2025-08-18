1c52a31ec615417b7805fb756ef446e7878e8ec1
Aaron <afritz1@users.noreply.github.com>
2025-08-18 16:01:00 -0700

    Added shader compilation batch file.

---
+++ windows/compile_shaders_glslc.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set SHADERS_DIRECTORY=..\data\shaders\
+
+for %%f in ("%SHADERS_DIRECTORY%*.vert" "%SHADERS_DIRECTORY%*.frag") do (
+    set "cmd[%i%]=glslc.exe "%%f" -o "%SHADERS_DIRECTORY%%%~nf.spv""
+    echo !cmd[%i%]!
+    call !cmd[%i%]!
+)
+
+echo.
+endlocal
+pause
