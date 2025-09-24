5bdd20a39aae11706914d0a906df7e944bc91814
Aaron <afritz1@users.noreply.github.com>
2025-09-24 12:19:25 -0700

    Use codesign --deep on macOS bundle.

--- macOS/fix_dylibs.sh
+++ macOS/fix_dylibs.sh
@@ -17,18 +17,15 @@ BREW_MOLTENVK_LIB=/usr/local/lib/libMoltenVK.dylib
 install_name_tool -change "${BREW_SDL2_LIB}" @executable_path/../Frameworks/libSDL2.dylib ${TES_APP_BUNDLE_PATH}/Contents/MacOS/${BINARY_NAME}
 install_name_tool -id @executable_path/../Frameworks/libSDL2.dylib ${TES_APP_BUNDLE_PATH}/Contents/Frameworks/libSDL2.dylib
 codesign --remove-signature ${TES_APP_BUNDLE_PATH}/Contents/Frameworks/libSDL2.dylib
-codesign -s - -o linker-signed ${TES_APP_BUNDLE_PATH}/Contents/Frameworks/libSDL2.dylib
 
 install_name_tool -change "${BREW_OPENAL_SOFT_LIB}" @executable_path/../Frameworks/libopenal.dylib ${TES_APP_BUNDLE_PATH}/Contents/MacOS/${BINARY_NAME}
 install_name_tool -id @executable_path/../Frameworks/libopenal.dylib ${TES_APP_BUNDLE_PATH}/Contents/Frameworks/libopenal.dylib
 codesign --remove-signature ${TES_APP_BUNDLE_PATH}/Contents/Frameworks/libopenal.dylib
-codesign -s - -o linker-signed ${TES_APP_BUNDLE_PATH}/Contents/Frameworks/libopenal.dylib
 
 # @todo if HAVE_WILDMIDI
 install_name_tool -change "${BREW_WILDMIDI_LIB}" @executable_path/../Frameworks/libWildMidi.dylib ${TES_APP_BUNDLE_PATH}/Contents/MacOS/${BINARY_NAME}
 install_name_tool -id @executable_path/../Frameworks/libWildMidi.dylib ${TES_APP_BUNDLE_PATH}/Contents/Frameworks/libWildMidi.dylib
 codesign --remove-signature ${TES_APP_BUNDLE_PATH}/Contents/Frameworks/libWildMidi.dylib
-codesign -s - -o linker-signed ${TES_APP_BUNDLE_PATH}/Contents/Frameworks/libWildMidi.dylib
 
 # @todo if HAVE_VULKAN
 install_name_tool -change "${BREW_VULKAN_LIB}" @executable_path/../Frameworks/libvulkan.dylib ${TES_APP_BUNDLE_PATH}/Contents/MacOS/${BINARY_NAME}
@@ -36,9 +33,9 @@ install_name_tool -change @rpath/libvulkan.1.dylib @executable_path/../Framework
 install_name_tool -add_rpath @loader_path ${TES_APP_BUNDLE_PATH}/Contents/Frameworks/libvulkan.dylib
 install_name_tool -id @executable_path/../Frameworks/libvulkan.dylib ${TES_APP_BUNDLE_PATH}/Contents/Frameworks/libvulkan.dylib
 codesign --remove-signature ${TES_APP_BUNDLE_PATH}/Contents/Frameworks/libvulkan.dylib
-codesign -s - -o linker-signed ${TES_APP_BUNDLE_PATH}/Contents/Frameworks/libvulkan.dylib
 
 install_name_tool -change "${BREW_MOLTENVK_LIB}" @loader_path/libMoltenVK.dylib ${TES_APP_BUNDLE_PATH}/Contents/Frameworks/libvulkan.dylib
 install_name_tool -id @loader_path/libMoltenVK.dylib ${TES_APP_BUNDLE_PATH}/Contents/Frameworks/libMoltenVK.dylib
 codesign --remove-signature ${TES_APP_BUNDLE_PATH}/Contents/Frameworks/libMoltenVK.dylib
-codesign -s - -o linker-signed ${TES_APP_BUNDLE_PATH}/Contents/Frameworks/libMoltenVK.dylib
+
+codesign -s - --deep -o linker-signed "${TES_APP_BUNDLE_PATH}"
