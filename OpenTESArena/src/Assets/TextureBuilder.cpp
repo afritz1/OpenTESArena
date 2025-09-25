a5d90985547ac807d093295766591e5fd9e42879
Aaron <afritz1@users.noreply.github.com>
2025-09-25 10:12:12 -0700

    Added support for high color TextureBuilder.

--- OpenTESArena/src/Assets/TextureBuilder.cpp
+++ OpenTESArena/src/Assets/TextureBuilder.cpp
@@ -21,6 +21,18 @@ void TextureBuilder::initPaletted(int width, int height, const uint8_t *texels)
 	std::copy(texels, texels + texelCount, reinterpret_cast<uint8_t*>(this->bytes.begin()));
 }
 
+void TextureBuilder::initHighColor(int width, int height, const uint16_t *texels)
+{
+	this->width = width;
+	this->height = height;
+	this->bytesPerTexel = 2;
+
+	const int texelCount = width * height;
+	const int byteCount = texelCount * 2;
+	this->bytes.init(byteCount);
+	std::copy(texels, texels + texelCount, reinterpret_cast<uint16_t*>(this->bytes.begin()));
+}
+
 void TextureBuilder::initTrueColor(int width, int height, const uint32_t *texels)
 {
 	this->width = width;
@@ -37,6 +49,12 @@ Span2D<const uint8_t> TextureBuilder::getTexels8() const
 {
 	DebugAssert(this->bytesPerTexel == 1);
 	return Span2D<const uint8_t>(reinterpret_cast<const uint8_t*>(this->bytes.begin()), this->width, this->height);
+}
+
+Span2D<const uint16_t> TextureBuilder::getTexels16() const
+{
+	DebugAssert(this->bytesPerTexel == 2);
+	return Span2D<const uint16_t>(reinterpret_cast<const uint16_t*>(this->bytes.begin()), this->width, this->height);
 }
 
 Span2D<const uint32_t> TextureBuilder::getTexels32() const
